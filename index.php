f399b3921ccac5211e2736a978192edd51de9214
Malcolm Bellman <mbellman@nwe.com>
2015-06-22 17:37:03 -0700

    Basic setup

---
+++ index.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Microorganisms</title>
+        <style type="text/css">
+            @import url('css/style.css');
+        </style>
+
+        <script src="js/jquery.min.js"></script>
+        <script src="js/main.js"></script>
+    </head>
+    <body>
+        <main>
+        </main>
+    </body>
+</html>
